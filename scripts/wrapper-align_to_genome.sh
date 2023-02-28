d4ba5a9e92bfb6f198bb760965e53e1e5f06ef38
MeaghanClark <meaghaniclark@gmail.com>
2023-02-28 11:53:36 -0500

    updated scripts to align sequencing data to reference genome for running on the hpcc

--- scripts/wrapper-align_to_genome.sh
+++ scripts/wrapper-align_to_genome.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 		
-# Last updated 04/16/2020 by MI Clark, originally written by R Toczydlowski 
+# Last updated 02/28/2023 by MI Clark, originally written by R Toczydlowski 
 
-# this version of the script is written if you have multiple sequencing lanes for the same individual. If you use the other script, 
-# you will overwrite output files due to naming scheme. This script produces files with more annoying names, 
-# but will not overwrite files. 
+# This version of the script is written if you have multiple sequencing lanes for the same individual, it avoids overwriting 
+# output files due to naming scheme. This script produces files with annoying names, but will not overwrite files. Give and take. 
 #
 
-#  run from project directory (where you want output directory to be created)
+# Run from project directory (where you want output directory to be created)
 
-#define variables:
-storagenode=/mnt/home/clarkm89 #path to top level of dir where input/output files live
-scratchnode=/mnt/scratch/clarkm89/massasauga_alignments_temp
-
-jobname=run-align_21 #label for SLURM book-keeping 
-logfilesdir=logfiles_aligntogenome #name of directory to create and then write log files to
+# define upper variables
+date=$(date +%m%d%Y)
+jobname=run-align #label for SLURM book-keeping 
 executable=./scripts/align_to_genome.sbatch #script to run 
+run_name=EMR_WGS #label to use on output files
 
+#define dirs:
+homedir=/mnt/home/clarkm89
+storagenode=/mnt/research/Fitz_Lab/projects/massasauga # Path to top level of dir where sequencing data, and where final alignments files will be stored
+scratchnode=/mnt/scratch/clarkm89/massasauga_alignments_temp # path to scratch dir where temp files will be stored
+logfilesdir=log_aligntogenome_${date} #name of directory to create and then write log files to
+
+# define running variables
 cpus=6 #number of CPUs to request/use per dataset
 ram_per_cpu=2G #amount of RAM to request/use per CPU CHANGE
-
 list_of_seqfiles=align_input.txt #list with names of seq files we want to align 
-run_name=massasauga #label to use on output files
-reference=$storagenode/$run_name/reference/Scatenatus_HiC_v1.1.fasta #filepath of reference file
+reference=$homedir/$run_name/reference/Scatenatus_HiC_v1.1.fasta #filepath of reference file
 
 #---------------------------------------------------------
 
@@ -42,8 +44,8 @@ do
 	
 	#define path to where input sequence data live for this dataset
 	#and where clean/processed reads should be written to
-	indir=$storagenode/$run_name/Rawdata/$ind_name
-	outdir=$storagenode/$run_name/alignments
+	indir=$storagenode/EMR_WGS_Rawdata/$ind_name
+	outdir=$storagenode/EMR_WGS_alignments_02282023
 	
 	#if input directory doesn't contain at least 1 .gz file; print warning, otherwise proceed with files that are there
 	n_inputfiles=($(ls $indir/*.gz | wc -l))
@@ -56,8 +58,8 @@ do
 			--export=REFERENCE=$reference,FORWARDREAD=$forwardread,REVERSEREAD=$reverseread,CPUS=$cpus,RUN_NAME=$run_name,SAMPLE_NAME=$sample_name,STORAGENODE=$storagenode,SCRATCHNODE=$scratchnode,INDIR=$indir,OUTDIR=$outdir,LOGFILESDIR=$logfilesdir \
 			--cpus-per-task=$cpus \
 			--mem-per-cpu=$ram_per_cpu \
-			--output=./$logfilesdir/${jobname}_${run_name}_${sample_name}_%A.out \
-			--error=./$logfilesdir/${jobname}_${run_name}_${sample_name}_%A.err \
+			--output=./$logfilesdir/${jobname}_${sample_name}_%A.out \
+			--error=./$logfilesdir/${jobname}_${sample_name}_%A.err \
 			--time=22:00:00 \
 			$executable
 			
