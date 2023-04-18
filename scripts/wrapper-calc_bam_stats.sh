33633c7b9b091b62fd1c77ca960aac1fad1e44bf
MeaghanClark <meaghaniclark@gmail.com>
2023-04-18 09:59:07 -0400

    adding list of bamstats files generated by calc_bam_stats scripts

--- scripts/wrapper-calc_bam_stats.sh
+++ scripts/wrapper-calc_bam_stats.sh
@@ -11,21 +11,21 @@ executable=/$HOME/EMR_WGS/scripts/calc_bam_stats.sbatch
 # define input and output files
 reference=/mnt/research/Fitz_Lab/ref/massasauga/EMR_ref_2021/Scatenatus_HiC_v1.1.fasta
 inbam=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/mask/EMR_all_qc.bam
-bamstats=/mnt/research/Fitz_Lab/software/ngsQC/bamstats
+bamstats=/mnt/research/Fitz_Lab/software/ngsQC/bamstats/bamstats
 
 # define log file directory
 date=$(date +%m%d%Y)
 logfilesdir=$HOME/EMR_WGS/log_bamstats_${date}
 
 # define running vars
-cpus=4
-ram_per_cpu=12
+cpus=1
+ram_per_cpu=25G
 
 #check if logfiles directory has been created in submit dir yet; if not, make one
 if [ ! -d $logfilesdir ]; then mkdir $logfilesdir; fi
 
 sbatch --job-name=$jobname \
---array=1..98 \
+--array=1-98 \
 --export=REFERENCE=$reference,CPUS=$cpus,RUN_NAME=$run_name,BAMSTATS=$bamstats,INBAM=$inbam,LOGFILESDIR=$logfilesdir,DATE=$date \
 --cpus-per-task=$cpus \
 --mem-per-cpu=$ram_per_cpu \
@@ -34,6 +34,6 @@ sbatch --job-name=$jobname \
 --time=24:00:00 \
 $executable
 
-echo I submitted to normalize my bcf file!
+echo I submitted an array job to run bamstats on ${inbam}!
 echo ----------------------------------------------------------------------------------------
 
