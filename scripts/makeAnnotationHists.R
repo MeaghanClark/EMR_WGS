138f98d92d8dfc8cd1429fb85c1d318edc5bff32
MeaghanClark <meaghaniclark@gmail.com>
2023-05-26 15:30:49 -0400

    changes to annotation histogram files

--- scripts/makeAnnotationHists.R
+++ scripts/makeAnnotationHists.R
@@ -14,7 +14,7 @@ pdf(file = outFile, height = 6, width = 6)
 
 # depth histogram
 
-hist(dp[,3], main = "Histogram of DP")
+hist(dp[,3], main = "Histogram of DP", xlim = c(0,500), breaks = 1e5)
 abline(v = median(dp[,3]), col = "black", lwd = 2)
 mtext(round(median(dp[,3])), at = median(dp[,3]))
 
@@ -38,27 +38,19 @@ legend("topright", c("+/- 0.5", "+/- 0.25", "x2"), col = c("red", "blue", "green
 print(paste0("Wow! I made a histogram from depth data "))
 print(paste0("The median is ", median(dp[,3])))
 
-# het histogram 
-hist(na.omit(as.numeric(het[,3])), main = "Histogram of het")
-abline(v = median(het[,3]), col = "black", lwd = 2)
-mtext(round(median(het[,3]), digits = 3), at = median(het[,3]))
+# het histogram
+het_vec <- na.omit(as.numeric(het[,3])) 
+hist(het_vec, main = "Histogram of het")
+abline(v = median(het_vec), col = "black", lwd = 2)
+mtext(round(median(het_vec), digits = 3), at = median(het_vec))
 
-abline(v = (median(het[,3])*1.5), col = "red", lwd = 2)
-mtext(round(median(het[,3])*1.5, digits = 3), at = (median(het[,3])*1.5), col = "red")
+abline(v = (median(het_vec)*0.5), col = "red", lwd = 2)
+mtext(round(median(het_vec)*0.5, digits = 3), at = (median(het_vec)*0.5), col = "red")
 
-abline(v = (median(het[,3])*0.5), col = "red", lwd = 2)
-mtext(round(median(het[,3])*0.5, digits = 3), at = (median(het[,3])*0.5), col = "red")
+abline(v = (median(het_vec)*0.75), col = "blue", lwd = 2)
+mtext(round(median(het_vec)*0.75, digits = 3), at = (median(het_vec)*0.75), col = "blue")
 
-abline(v = (median(het[,3])*0.75), col = "blue", lwd = 2)
-mtext(round(median(het[,3])*0.75, digits = 3), at = (median(het[,3])*0.75), col = "blue")
-
-abline(v = (median(het[,3])*1.25), col = "blue", lwd = 2)
-mtext(round(median(het[,3])*1.25, digits = 3), at = (median(het[,3])*1.25), col = "blue")
-
-abline(v = (median(het[,3])*2), col = "green", lwd = 2)
-mtext(round(median(het[,3])*2, digits = 3), at = (median(het[,3])*2), col = "green")
-
-legend("topright", c("+/- 0.5", "+/- 0.25", "x2"), col = c("red", "blue", "green"), lty = 1, lwd = 2)
+legend("topleft", c("+/- 0.5", "+/- 0.25", "x2"), col = c("red", "blue", "green"), lty = 1, lwd = 2)
 
 print(paste0("Wow! I made a histogram from het data ")) 
 
