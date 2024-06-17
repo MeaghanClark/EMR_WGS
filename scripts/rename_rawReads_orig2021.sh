9912e39fd585ca96ca2e5b048a8473d1f9233895
MeaghanClark <meaghaniclark@gmail.com>
2024-06-17 17:12:58 -0400

    adding renaming script for original 2021 genomes

---
+++ scripts/rename_rawReads_orig2021.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Fitz 2021 genomes
+# Path to the directory containing the files
+FILE_DIR="/mnt/scratch/clarkm89/EMR_WGS_2020_RawData/rawData"
+# Path to the ID mapping file
+
+while read file; do
+	#for file in "$FILE_DIR"/"$initialID"_*; do
+		echo $file
+        # Extract the forward/reverse read info from the filename
+        importantinfo=$(echo "$file" | sed 's/.*_\([[:digit:]]\).fq.gz/\1/')
+        # Construct the new filename
+        finalID=$(echo $file | sed -E 's/^([A-Z_]+[0-9]+).*/\1/')
+        echo $finalID
+        echo $importantinfo
+        newfile="${FILE_DIR}/${finalID}_${importantinfo}.fastq.gz"
+        echo $newfile
+        echo "#####"
+		# mv "$file" "$newfile"
+	#done
+done < "${FILE_DIR}/files.txt"
+
+
+
