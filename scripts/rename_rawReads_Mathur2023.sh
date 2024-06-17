e14fb0567baa5df900672bc852d5ddaf34b4a47c
MeaghanClark <meaghaniclark@gmail.com>
2024-06-17 16:33:01 -0400

    separating renaming scripts for data sources

--- scripts/rename_rawReads_Mathur2023.sh
+++ scripts/rename_rawReads_Mathur2023.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
+# Mathur_etal_2023 genomes
 # Path to the directory containing the files
 FILE_DIR="/mnt/scratch/clarkm89/EMR_WGS_Mathur2023"
 # Path to the ID mapping file
 ID_MAPPING_FILE="/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/Mathur2023_ids.txt"
+
 
 while read initialID finalID; do
 	for file in "$FILE_DIR"/"$initialID"_*; do
