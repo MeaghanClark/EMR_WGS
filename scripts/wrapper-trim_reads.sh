0a7659b61b3d76e019e8146def6e8ed0913e3ffe
MeaghanClark <meaghaniclark@gmail.com>
2024-06-17 20:47:43 -0400

    changing encoding for trim adatpers key

--- scripts/wrapper-trim_reads.sh
+++ scripts/wrapper-trim_reads.sh
@@ -10,13 +10,12 @@
 
 # define high level variables
 jobname=trim_reads
-array_key=/mnt/research/Fitz_Lab/projects/massasauga/WGS/scripts/keys/rawData_FRcols.txt # list of raw data with forward and reverse reads for the same individual on the same line
+array_key=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/rawData_FRcols.txt # list of raw data with forward and reverse reads for the same individual on the same line
 rundate=$(date +%m%d%Y)
 
 # define directories
-indir=/mnt/research/Fitz_Lab/projects/massasauga/WGS/rawData/
 outdir=/mnt/scratch/clarkm89/EMR_WGS/processedReads/  # outdir on scratch because of storage limits
-logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/WGS/logs/trimReads
+logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/trimReads
  
 # make sure dir exists
 if [ ! -d $logfilesdir ]; then mkdir $logfilesdir; fi
@@ -29,11 +28,11 @@ ram_per_cpu=24G
 array_no=$(cat $array_key | wc -l)
 
 # define executable 
-executable=/path/to/run_fastQC.sbatch # CHANGE
+executable=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/trim_reads.sbatch
 
 sbatch --job-name=$jobname \
 	--array=1-$array_no \
-	--export=JOBNAME=$jobname,ARRAY_KEY=$array_key,CPUS=$cpus,RUNDATE=$rundate,EXECUTABLE=$executable,LOGFILESDIR=$logfilesdir,OUTDIR=$outdir,INDIR=$indir \
+	--export=JOBNAME=$jobname,ARRAY_KEY=$array_key,CPUS=$cpus,RUNDATE=$rundate,EXECUTABLE=$executable,LOGFILESDIR=$logfilesdir,OUTDIR=$outdir \
 	--cpus-per-task=$cpus \
 	--mem-per-cpu=$ram_per_cpu \
 	--output=$logfilesdir/${jobname}_${rundate}_%A-%a.out \
