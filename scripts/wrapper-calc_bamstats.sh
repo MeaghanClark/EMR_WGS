2e0deb9477e081497798c09611b1e33b69ea092b
MeaghanClark <meaghaniclark@gmail.com>
2024-06-24 10:59:06 -0400

    updating calc bamstats files

--- scripts/wrapper-calc_bamstats.sh
+++ scripts/wrapper-calc_bamstats.sh
@@ -3,7 +3,7 @@
 # wrapper-calc_bamstats.sh		
 # This script starts an array job to run bamstats on merged bam file from merge_bams.sbatch
 # The array job will start one job per chromosome grouping
-# Last updated 05/21/2024 by MI Clark, script format inspired by R Toczydlowski and T Linderoth
+# Last updated 06/24/2024 by MI Clark, script format inspired by R Toczydlowski and T Linderoth
 
 #  run from project directory (where you want output directory to be created)
 
@@ -12,12 +12,12 @@ date=$(date +%m%d%Y)
 jobname=bam_stats
 
 # define dirs
-chrom_list_dir=/path/to/dir/with/files/specifying/chroms # ***need to make chrom files
-logfilesdir=$HOME/EMR_WGS/log_bamstats_${date}
-outdir=/path/to/bamstats/output/directory # CHANGE
+chrom_list_dir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/chrom
+logfilesdir=/mnt/research/FitzLab/projects/massasauga/WGS/logs/${jobname}
+outdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/bamstats
 
 # define input and output files
-inbam=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/mask/EMR_all_qc.bam
+inbam=/mnt/scratch/clarkm89/EMR_WGS/bamstats/mega_bam.bam
 
 # define running vars
 cpus=1
@@ -26,7 +26,7 @@ array_no=$(ls $chrom_list_dir | wc -l) #***
 
 # define executable, reference and needed scripts
 reference=/mnt/research/Fitz_Lab/ref/massasauga/EMR_ref_2021/Scatenatus_HiC_v1.1.fasta
-executable=/$HOME/EMR_WGS/scripts/calc_bam_stats.sbatch
+executable=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/calc_bam_stats.sbatch
 bamstats=/mnt/research/Fitz_Lab/software/ngsQC/bamstats/bamstats
 
 #check if logfiles directory has been created in submit dir yet; if not, make one
@@ -39,7 +39,8 @@ sbatch --job-name=$jobname \
 --mem-per-cpu=$ram_per_cpu \
 --output=$logfilesdir/${jobname}_%A-%a.out \
 --error=$logfilesdir/${jobname}_%A-%a.err \
---time=24:00:00 \
+--time=48:00:00 \
+--account=bradburd \
 $executable
 
 echo I submitted an array job to run bamstats on ${inbam}!
