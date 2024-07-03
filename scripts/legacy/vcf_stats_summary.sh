266d12467b77c1335cfb1af420eac90165687cfb
MeaghanClark <meaghaniclark@gmail.com>
2024-07-03 11:53:38 -0400

    adding file to index bcf files

--- scripts/legacy/vcf_stats_summary.sh
+++ scripts/legacy/vcf_stats_summary.sh
@@ -2,31 +2,38 @@
 
 ########## SBATCH Lines for Resource Request ##########
 
-#SBATCH --time=12:00:00             # limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --time=72:00:00             # limit of wall clock time - how long the job will run (same as -t)
 #SBATCH --cpus-per-task=1         # number of CPUs (or cores) per task (same as -c)
 #SBATCH --mem-per-cpu=400G            # memory required per allocated CPU (or core)
 #SBATCH --job-name=vcf_stats_sum      # you can give your job a name for easier identification (same as -J)
-#SBATCH --output="/mnt/home/clarkm89/EMR_WGS/log_vcf_stats_sum/vcf_stats_sum_%A.out"
-#SBATCH --error="/mnt/home/clarkm89/EMR_WGS/log_vcf_stats_sum/vcf_stats_sum_%A.err"
+#SBATCH --output="/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/vcf_stats_sum/vcf_stats_sum_%A.out"
+#SBATCH --error="/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/vcf_stats_sum/vcf_stats_sum_%A.err"
+#SBATCH --account=bradburd
 
 ##########
 
 # load modules
 module purge
 module load powertools
-module load GCC/8.3.0 OpenMPI/3.1.4
-module load R/4.0.2
+module load BCFtools/1.19-GCC-13.2.0
 module list 
 
+#find ${OUTFILE} > $OUTLIST # this line should crease a file OUTLIST that contains the path + name of OUTFILE 
+
 # define variables
-EXEC='$HOME/EMR_WGS/scripts/qualSummaryStats.R'
-STATLIST='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_vcfstats_list.txt'
-OUTFILE='$HOME/EMR_WGS/variants/masks/EMR_norm.vcfstats.summary'
+EXEC='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/qualSummaryStats.R'
+OUTREPORT='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_drop_norm_rename.vcfstats.summary' 
+OUTPLOT='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_drop_norm_rename_vcfstats_summary.pdf' 
+OUTLIST='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_vcfstats_list.txt' 
+
 
 # run code
-CMD="$EXEC $STATLIST $OUTFILE 3 4 5 6 7 8 9 10 11 12 13 14 15"
+CMD="$EXEC $OUTLIST $OUTREPORT $OUTPLOT 3 4 5 6 7 8 9 10 11 12 13 14" 
 printf "\n%s\n\n" "$CMD"
 eval $CMD
+
+
+wait
 
 #print some environment variables to stdout for records
 echo ----------------------------------------------------------------------------------------
--- scripts/vcf_stats.sh
+++ scripts/vcf_stats.sh
@@ -24,26 +24,11 @@ BCF='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/EMR_WGS_drop_no
 OUTFILE='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_WGS_drop_norm_rename.vcfstats'
 
 # run code
-CMD="(printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' 'CHR' 'POS' 'DP' 'NS' 'QUAL' 'MQ' 'RPBZ' 'MQBZ' 'BQBZ' 'FS' 'ExcHet' 'PV4_STRAND' 'PV4_BASEQ' 'PV4_MAPQ' 'PV4_POS' && bcftools query -f '%CHROM\t%POS\t%INFO/DP\t%INFO/NS\t%QUAL\t%MQ\t%INFO/RPBZ\t%INFO/MQBZ\t%INFO/BQBZ\t%INFO/FS\t%INFO/ExcHet\t%INFO/PV4{0}\t%INFO/PV4{1}\t%INFO/PV4{2}\t%INFO/PV4{3}\n' $BCF) > $OUTFILE"
+
+CMD="(printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' 'CHR' 'POS' 'DP' 'NS' 'QUAL' 'MQ' 'RPBZ' 'MQBZ' 'BQBZ' 'ExcHet' 'PV4_STRAND' 'PV4_BASEQ' 'PV4_MAPQ' 'PV4_POS' && bcftools query -f '%CHROM\t%POS\t%INFO/DP\t%INFO/NS\t%QUAL\t%MQ\t%INFO/RPBZ\t%INFO/MQBZ\t%INFO/BQBZ\t%INFO/ExcHet\t%INFO/PV4{0}\t%INFO/PV4{1}\t%INFO/PV4{2}\t%INFO/PV4{3}\n' $BCF) > $OUTFILE"
 
 printf "\n%s\n\n" "$CMD"
 eval $CMD
-
-wait
-
-#find ${OUTFILE} > $OUTLIST # this line should crease a file OUTLIST that contains the path + name of OUTFILE 
-
-# define variables
-EXEC='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/qualSummaryStats.R'
-OUTREPORT='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_drop_norm_rename.vcfstats.summary' 
-OUTPLOT='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_drop_norm_rename_vcfstats_summary.pdf' 
-OUTLIST='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_vcfstats_list.txt' 
-
-# run code
-CMD2="$EXEC $OUTLIST $OUTREPORT $OUTPLOT 3 4 5 6 7 8 9 10 11 12 13 14 15"
-printf "\n%s\n\n" "$CMD2"
-eval $CMD2
-
 
 wait
 
