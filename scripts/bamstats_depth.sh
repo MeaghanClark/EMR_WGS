7436c22268b8a2c808c07a9d23474122c94c5c94
MeaghanClark <meaghaniclark@gmail.com>
2024-07-06 18:48:17 -0400

    Adding scripts to viz depth separately

---
+++ scripts/bamstats_depth.sh
@@ -0,0 +1,40 @@
+#!/bin/bash --login
+
+########## SBATCH Lines for Resource Request ##########
+#SBATCH --time=12:00:00             # limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --cpus-per-task=1         # number of CPUs (or cores) per task (same as -c)
+#SBATCH --mem-per-cpu=200G            # memory required per allocated CPU (or core)
+#SBATCH --job-name=bamstats_depth     # you can give your job a name for easier identification (same as -J)
+#SBATCH --output="/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/bamstats_sum/bamstats_depth%A.out"
+#SBATCH --error="/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/bamstats_sum/bamstats_depth%A.err"
+#SBATCH --account=bradburd
+##########
+
+# load modules
+module purge
+module load powertools
+module load R/4.3.2-gfbf-2023a
+module list 
+
+#find ${OUTFILE} > $OUTLIST # this line should crease a file OUTLIST that contains the path + name of OUTFILE 
+
+# define variables
+EXEC='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/qualDepth.R'
+DEPTH_FILE='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/bamstats/EMR_bamstats_depth.txt'
+ROBJ='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/bamstats/EMR_ALL_bamstats_depth.Robj' 
+REPORT_FILE='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/bamstats/EMR_ALL_qc_depth.pdf' 
+
+# run code
+CMD="$EXEC $DEPTH_FILE $ROBJ $REPORT_FILE" 
+printf "\n%s\n\n" "$CMD"
+eval $CMD
+
+
+wait
+
+#print some environment variables to stdout for records
+echo ----------------------------------------------------------------------------------------
+echo PRINTING SUBSET OF ENVIRONMENT VARIABLES:
+(set -o posix ; set | grep -v ^_ | grep -v ^EB | grep -v ^BASH | grep -v PATH | grep -v LS_COLORS)
+
+echo ----------------------------------------------------------------------------------------
