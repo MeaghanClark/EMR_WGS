0902770324cd0b4cc152f20993707d588db4c021
MeaghanClark <meaghaniclark@gmail.com>
2024-07-10 00:49:26 -0400

    updated annotation scripts to use -r instead of -R flag

--- scripts/annotateVCF.sh
+++ scripts/annotateVCF.sh
@@ -12,10 +12,11 @@ EXEC='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/insertAnnotatio
 BCF='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/EMR_WGS_drop_norm.bcf.gz' # Merged bcf file! 
 OUTFILE='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/annotate_vcf/EMR_drop_norm_annotated1_${SLURM_ARRAY_TASK_ID}.vcf.gz' # annotated VCF! 
 GRPFILE='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/EMR_groups_qc.txt' #CHANGE
-REGFILE='${CHROM_LIST_DIR}/annotate_chunk_${SLURM_ARRAY_TASK_ID}.txt'
+REGFILE='${CHROM_LIST_DIR}/scaf_list_${SLURM_ARRAY_TASK_ID}.txt'
 BEDFILE='/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/masks/EMR_mask_fail_sorted.bed' # output from gen_masks.sbatch, double check file name! 
 
-CMD="bcftools view -R $REGFILE --no-version $BCF | $EXEC --dpbounds 1885,5656 --hetbound 1e-4 --bed $BEDFILE --overwrite --genorep $GRPFILE | bgzip > $OUTFILE" 
+# trying -r, annotate by scaffold 
+CMD="bcftools view -r $REGFILE --no-version $BCF | $EXEC --dpbounds 1885,5656 --hetbound 1e-4 --bed $BEDFILE --overwrite --genorep $GRPFILE | bgzip > $OUTFILE" 
 
 printf "\n%s\n\n" "$CMD"
 eval $CMD
