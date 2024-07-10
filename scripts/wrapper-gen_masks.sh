d0dff962c9b177bac183f7af8939bd1aaad51352
MeaghanClark <meaghaniclark@gmail.com>
2024-07-10 09:43:06 -0400

    gen_masks by scaffold

--- scripts/wrapper-gen_masks.sh
+++ scripts/wrapper-gen_masks.sh
@@ -12,7 +12,7 @@ date=$(date +%m%d%Y)
 jobname=gen_mask
 
 # define dirs
-chrom_list_dir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/chrom_200
+chrom_list_dir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scaffolds
 logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/${jobname}
 
 # define running vars
