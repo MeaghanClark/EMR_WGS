178ca706be79b9e53593e98e8fdbb883e1b948ed
MeaghanClark <meaghaniclark@gmail.com>
2024-07-10 00:49:26 -0400

    updated annotation scripts to use -r instead of -R flag

--- scripts/wrapper-annotateVCF.sh
+++ scripts/wrapper-annotateVCF.sh
@@ -13,7 +13,7 @@ date=$(date +%m%d%Y)
 
 #define dirs:
 logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/${jobname} 
-chrom_list_dir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/annotate_chunks
+chrom_list_dir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scaffolds
 indir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/
 outdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/
 
@@ -36,7 +36,7 @@ sbatch --job-name=$jobname \
 		--mem=$total_mem \
 		--output=$logfilesdir/${jobname}_${date}_%A-%a.out \
 		--error=$logfilesdir/${jobname}_${date}_%A-%a.err \
-		--time=12:00:00 \
+		--time=168:00:00 \
 		--account=bradburd \
 		$executable
 
