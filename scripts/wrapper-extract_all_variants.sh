14bdab4a6b98a264ad87b9bdb14fa1452646bfd7
MeaghanClark <meaghaniclark@gmail.com>
2024-07-14 17:16:54 -0400

    updated snp extract scrips

--- scripts/wrapper-extract_all_variants.sh
+++ scripts/wrapper-extract_all_variants.sh
@@ -14,12 +14,12 @@ date=$(date +%m%d%Y)
 #define dirs:
 logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/${jobname} 
 chrom_list_dir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scaf_single_line
-indir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/annotate_vcf
+indir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/annotate_vcf_MI
 outdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/
 
 # define slurm job details
 cpus=1
-total_mem=12G
+total_mem=20G
 array_no=$(ls $chrom_list_dir | wc -l) #***
 
 # define executable and reference files
@@ -30,7 +30,7 @@ if [ ! -d $logfilesdir ]; then mkdir $logfilesdir; fi
 if [ ! -d $outdir ]; then mkdir $outdir; fi
 
 sbatch --job-name=$jobname \
-		--array=4-$array_no \
+		--array=8-$array_no \
 		--export=CPUS=$cpus,RUN_NAME=$run_name,CHROM_LIST_DIR=$chrom_list_dir,LOGFILESDIR=$logfilesdir,DATE=$date,INDIR=$indir,OUTDIR=$outdir \
 		--cpus-per-task=$cpus \
 		--mem=$total_mem \
