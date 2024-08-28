e8edd3f31b407af8f70ce8f1fcdcae59c3299e80
MeaghanClark <meaghaniclark@gmail.com>
2024-08-28 10:40:03 -0400

    adding scripts to filter bams

---
+++ scripts/wrapper-filter_bams.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# wrapper-align_to_genome.sh <seq_run>		
+# This script starts an array job to filter alignment files from "wrapper-align_to_genome.sbatch"
+# Last updated 08/27/2024 by MI Clark, originally written by R Toczydlowski 
+
+# define high level variables
+date=$(date +%m%d%Y)
+echo $date 
+
+seq_run=$1 # should be "original" or "resequencing"
+
+if [[ $seq_run == original ]]; then 
+	jobname=bam_flt_orig #label for SLURM book-keeping 
+	array_key=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/trimmed_reads_orig.txt # file with names of trimmed reads, one ind per line, reads separated by blank space
+
+elif [[ $seq_run == resequencing ]]; then 
+	jobname=bam_flt_reseq #label for SLURM book-keeping 
+	array_key=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/trimmed_reads_AugReseq.txt # file with names of trimmed reads, one ind per line, reads separated by blank space
+else echo "ERROR, seq_run is invalid"
+
+	# "trimmed_reads_paired.txt" will align paired-end reads from the original run   
+	# "trimmed_reads_Aug_reseq.txt" will align paired-end reads from the August resequencing run
+fi 
+
+#define dirs:
+outdir=/mnt/scratch/clarkm89/EMR_WGS/alignments/${jobname}
+scratchnode=/mnt/scratch/clarkm89/EMR_WGS/alignmentsTemp/${jobname} # path to scratch dir where temp files will be stored
+logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/${jobname} #name of directory to create and then write log files to
+
+#check if directories have been created; if not, make 
+if [ ! -d $logfilesdir ]; then mkdir $logfilesdir; fi
+if [ ! -d $outdir ]; then mkdir $outdir; fi
+if [ ! -d $scratchnode ]; then mkdir $scratchnode; fi
+
+# define slurm job details
+cpus=1 #number of CPUs to request/use per dataset
+ram_per_cpu=12G #amount of RAM to request/use per CPU CHANGE
+array_no=$(cat $array_key | wc -l)
+
+# define executable and reference genome 
+executable=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/align_to_genome.sbatch #script to run 
+
+#---------------------------------------------------------
+# required exports to executable: 
+#	ARRAY_KEY
+# 	REFERENCE
+# 	CPUS
+#	OUTDIR
+#	INDIR
+#	SCRATCHNODE
+
+#       --array=1-$array_no \
+#submit job to cluster
+sbatch --job-name=$jobname \
+		--array=1-$array_no \
+		--export=ARRAY_KEY=$array_key,CPUS=$cpus,SCRATCHNODE=$scratchnode,OUTDIR=$outdir,LOGFILESDIR=$logfilesdir \
+		--cpus-per-task=$cpus \
+		--mem-per-cpu=$ram_per_cpu \
+		--output=$logfilesdir/${jobname}_${date}_%A-%a.out \
+		--error=$logfilesdir/${jobname}_${date}_%A-%a.err \
+		--time=72:00:00 \
+		--account=bradburd \
+		$executable
+		
+echo submitted a job to align reads in $indir to $reference
+
+echo ----------------------------------------------------------------------------------------
+echo My executable is $executable		
--- scripts/wrapper-merge_bams.sh
+++ scripts/wrapper-merge_bams.sh
@@ -6,44 +6,40 @@
 
 # define high level vars
 jobname=bam_merge
+date=$(date +%m%d%Y)
+array_key=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/bams_to_merge.txt # file with file paths to bams to merge, one ind per line, reads separated by blank space
+
 
 # define dirs
-
+outdir=/mnt/scratch/clarkm89/EMR_WGS/alignments/${jobname}
+logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/${jobname} #name of directory to create and then write log files to
 
 #check if directories have been created; if not, make 
 if [ ! -d $logfilesdir ]; then mkdir $logfilesdir; fi
 if [ ! -d $outdir ]; then mkdir $outdir; fi
 if [ ! -d $scratchnode ]; then mkdir $scratchnode; fi
 
-# define slurm job details
-
-# define input and output files
-bams=$HOME/EMR_WGS/bam_list_path.txt
-outfile=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/mask/EMR_all_qc.bam
-
-# define log file directory
-date=$(date +%m%d%Y)
-logfilesdir=$HOME/EMR_WGS/log_merge_${date}
-
 # define running vars
 cpus=4
 ram_per_cpu=12
+array_no=$(cat $array_key | wc -l)
 
 # define executable and reference
 executable=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/merge_bams.sbatch
 reference=/mnt/scratch/clarkm89/EMR_ref_2024/GCA_039880765.1/GCA_039880765.1_rSisCat1_p1.0_genomic.fna #filepath of reference file
 
-#check if logfiles directory hassbeen created in submit dir yet; if not, make one
-if [ ! -d $logfilesdir ]; then mkdir $logfilesdir; fi
 
 sbatch --job-name=$jobname \
---export=REFERENCE=$reference,CPUS=$cpus,RUN_NAME=$run_name,BAMS=$bams,OUTFILE=$outfile,LOGFILESDIR=$logfilesdir,DATE=$date \
---cpus-per-task=$cpus \
---mem-per-cpu=$ram_per_cpu \
---output=$logfilesdir/${jobname}_%A.out \
---error=$logfilesdir/${jobname}_%A.err \
---time=24:00:00 \
-$executable
+		--array=1-$array_no \
+		--export=ARRAY_KEY=$array_key,REF=$reference,CPUS=$cpus,OUTDIR=$outdir,LOGFILESDIR=$logfilesdir \
+		--cpus-per-task=$cpus \
+		--mem-per-cpu=$ram_per_cpu \
+		--output=$logfilesdir/${jobname}_${date}_%A-%a.out \
+		--error=$logfilesdir/${jobname}_${date}_%A-%a.err \
+		--time=72:00:00 \
+		--account=bradburd \
+		$executable
+
 
 echo I submitted to merge all bams!
 echo ----------------------------------------------------------------------------------------
