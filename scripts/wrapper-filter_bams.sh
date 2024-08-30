1321eb682c5e9b82d0b28cd9abf652d17a611f97
MeaghanClark <meaghaniclark@gmail.com>
2024-08-30 15:25:36 -0400

    syncing

--- scripts/wrapper-filter_bams.sh
+++ scripts/wrapper-filter_bams.sh
@@ -11,9 +11,9 @@ echo $date
 seq_run=$1 # should be "original" or "resequencing"
 
 if [[ $seq_run == original ]]; then 
-	jobname=bam_flt_orig #label for SLURM book-keeping 
+	jobname=bam_flt_rerun #label for SLURM book-keeping 
 	indir=/mnt/scratch/clarkm89/EMR_WGS/alignmentsTemp/align_orig
-	array_key=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/trimmed_reads_orig.txt # file with names of trimmed reads, one ind per line, reads separated by blank space
+	array_key=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/trimmed_reads_rerun.txt # file with names of trimmed reads, one ind per line, reads separated by blank space
 
 elif [[ $seq_run == resequencing ]]; then 
 	jobname=bam_flt_reseq #label for SLURM book-keeping 
@@ -27,7 +27,7 @@ fi
 
 #define dirs:
 outdir=/mnt/scratch/clarkm89/EMR_WGS/alignments/${jobname}
-scratchnode=/mnt/scratch/clarkm89/EMR_WGS/alignmentsTemp/${jobname} # path to scratch dir where temp files will be stored
+scratchnode=/mnt/scratch/clarkm89/EMR_WGS/alignmentsTemp/bam_flt_orig # path to scratch dir where temp files will be stored
 logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/${jobname} #name of directory to create and then write log files to
 
 #check if directories have been created; if not, make 
@@ -36,8 +36,10 @@ if [ ! -d $outdir ]; then mkdir $outdir; fi
 if [ ! -d $scratchnode ]; then mkdir $scratchnode; fi
 
 # define slurm job details
-cpus=15 #number of CPUs to request/use per dataset
-mem=40G #amount of RAM to request/use per CPU CHANGE
+#cpus=15 #number of CPUs to request/use per dataset
+cpus=5
+#mem=40G #amount of RAM to request/use per CPU CHANGE
+mem=20G
 array_no=$(cat $array_key | wc -l)
 
 # define executable and reference genome 
@@ -55,7 +57,7 @@ executable=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/filter_bam
 #       --array=1-$array_no \
 #submit job to cluster
 sbatch --job-name=$jobname \
-		--array=1-$array_no \
+		--array=31,32 \
 		--export=ARRAY_KEY=$array_key,CPUS=$cpus,SCRATCHNODE=$scratchnode,OUTDIR=$outdir,INDIR=$indir,LOGFILESDIR=$logfilesdir \
 		--cpus-per-task=$cpus \
 		--mem=$mem \
