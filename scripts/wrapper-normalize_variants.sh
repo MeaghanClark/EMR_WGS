b1b49b73148c69db1e3bf6c009b5d7b645995a31
MeaghanClark <meaghaniclark@gmail.com>
2024-09-03 14:00:21 -0700

    adding scaffold files to the repo and updating normalization scripts

--- scripts/wrapper-normalize_variants.sh
+++ scripts/wrapper-normalize_variants.sh
@@ -3,7 +3,7 @@
 # wrapper-normalize_variants.sh		
 # This script starts an array job normalize variants in raw bcf files
 # The array job will start one job per BCF in bcflist
-# Last updated 07/01/2024 by MI Clark, script format inspired by R Toczydlowski 
+# Last updated 09/03/2024 by MI Clark, script format inspired by R Toczydlowski 
 
 #  run from project directory (where you want output directory to be created)
 
@@ -14,30 +14,32 @@ date=$(date +%m%d%Y)
 
 #define dirs:
 logfilesdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/${jobname} 
-#chrom_list_dir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/chrom 
-indir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/
-outdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/
+chrom_list_dir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/keys/scaffolds # make scaffolds dir and list --> 
+indir=/mnt/scratch/clarkm89/EMR_WGS/variants/
+outdir=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/norm_byScaf
 
-# define slurm job details
-cpus=4
-total_mem=48G
-#array_no=$(ls $chrom_list_dir | wc -l) #***
-
-# define executable and reference files
-reference=/mnt/research/Fitz_Lab/ref/massasauga/EMR_ref_2021/Scatenatus_HiC_v1.1.fasta 
-executable=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/normalize_variants.sbatch
-
-#check if logfiles directory has been created in submit dir yet; if not, make one
+#check if directories have been created yet; if not, make one
 if [ ! -d $logfilesdir ]; then mkdir $logfilesdir; fi
 if [ ! -d $outdir ]; then mkdir $outdir; fi
 
+# define slurm job details
+cpus=6
+total_mem=50G
+array_no=$(ls $chrom_list_dir | wc -l) 
+
+# define executable and reference files
+reference=/mnt/scratch/clarkm89/EMR_ref_2024/GCA_039880765.1/GCA_039880765.1_rSisCat1_p1.0_genomic.fna #filepath of reference file
+executable=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/scripts/normalize_variants.sbatch
+
+
 sbatch --job-name=$jobname \
-		--export=REFERENCE=$reference,CPUS=$cpus,RUN_NAME=$run_name,LOGFILESDIR=$logfilesdir,DATE=$date,INDIR=$indir,OUTDIR=$outdir \
+		--array=1-$array_no \
+		--export=REFERENCE=$reference,CPUS=$cpus,CHROM_LIST_DIR=$chrom_list_dir,RUN_NAME=$run_name,LOGFILESDIR=$logfilesdir,DATE=$date,INDIR=$indir,OUTDIR=$outdir \
 		--cpus-per-task=$cpus \
 		--mem=$total_mem \
 		--output=$logfilesdir/${jobname}_${date}_%A-%a.out \
 		--error=$logfilesdir/${jobname}_${date}_%A-%a.err \
-		--time=72:00:00 \
+		--time=96:00:00 \
 		--account=bradburd \
 		$executable
 
