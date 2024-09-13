e5c27caa7f54a3c3ce4986c0479339a07ff232a3
MeaghanClark <meaghaniclark@gmail.com>
2024-09-13 17:18:37 -0400

    updating extract_variants_no_maf.sh

--- scripts/extract_variants_no_maf.sh
+++ scripts/extract_variants_no_maf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash --login
 
 ########## SBATCH Lines for Resource Request ##########
-#SBATCH --time=168:00:00             # limit of wall clock time - how long the job will run (same as -t)
-#SBATCH --array=99-103
+#SBATCH --time=72:00:00             # limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --array=1-200
 #SBATCH --cpus-per-task=1      # number of CPUs (or cores) per task (same as -c)
-#SBATCH --mem-per-cpu=100G            # memory required per allocated CPU (or core)
+#SBATCH --mem-per-cpu=50G            # memory required per allocated CPU (or core)
 #SBATCH --job-name=ext_var_nomaf    # you can give your job a name for easier identification (same as -J)
 #SBATCH --output="/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/ext_var_nomaf/ext_var_nomaf_%A_%a.out" 
 #SBATCH --error="/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/logs/ext_var_nomaf/ext_var_nomaf_%A_%a.err"
@@ -12,7 +12,7 @@
 ##########
 
 EXEC='/mnt/research/Fitz_Lab/projects/posk/variants/vcf/scripts/extractVariants.pl'
-INVCF=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/annotate_vcf/EMR_drop_norm_annotated_${SLURM_ARRAY_TASK_ID}.vcf.gz # annotated allsites VCF
+INVCF=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/annotated_vcf/EMR_annotated_${SLURM_ARRAY_TASK_ID}.vcf.gz # annotated allsites VCF
 OUTVCF=/mnt/research/Fitz_Lab/projects/massasauga/EMR_WGS/variants/variants_noMAF/EMR_variants_nomaf_${SLURM_ARRAY_TASK_ID}.vcf.gz  # nomaf variant sites VCF
 
 CMD="$EXEC --skip_af $INVCF | bgzip > $OUTVCF"
